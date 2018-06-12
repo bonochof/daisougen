2534e974cb950919f386b4209fc32d56b6ff3b7a
bonochof <ryos427@yahoo.co.jp>
2018-06-13 00:14:03 +0900

    add main file

---
+++ www.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define w int
+#define ww main
+#define www printf
+#define wwww "Hello world\n"
+#define wwwww return
+#define wwwwww 0
+#define wwwwwww (
+#define wwwwwwww )
+#define wwwwwwwww {
+#define wwwwwwwwww }
+#define wwwwwwwwwww ;
+
+w ww wwwwwww wwwwwwww wwwwwwwww
+  www wwwwwww wwww wwwwwwww wwwwwwwwwww
+  wwwww wwwwww wwwwwwwwwww
+wwwwwwwwww
+
